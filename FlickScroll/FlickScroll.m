baa1ddb5f34395973726e205658b63b7505c84c8
Mathew Huusko V <mhuusko5@gmail.com>
2015-01-03 12:57:00 -0500

    Tuned + with preferences

--- FlickScroll/FlickScroll.m
+++ FlickScroll/FlickScroll.m
@@ -23,70 +23,92 @@ static void M5SwizzleMethod(Class clazz, SEL original, SEL alternate) {
 
 @implementation UIScrollView (FlickScroll)
 
-const void* M5_scrollTouchCount = &M5_scrollTouchCount;
-const void* M5_scrollTouchPoint = &M5_scrollTouchPoint;
+const void* scrollTouchCount = &scrollTouchCount;
+const void* scrollTouchPoint = &scrollTouchPoint;
+const void* scrollTouchVelocity = &scrollTouchVelocity;
 
 - (void)M5_scrollViewWillBeginDragging {
-    objc_setAssociatedObject(self, M5_scrollTouchCount, @(self.panGestureRecognizer.numberOfTouches), OBJC_ASSOCIATION_RETAIN);
-    objc_setAssociatedObject(self, M5_scrollTouchPoint, [NSValue valueWithCGPoint:[self.panGestureRecognizer locationInView:self]], OBJC_ASSOCIATION_RETAIN);
+    objc_setAssociatedObject(self, scrollTouchCount, @(self.panGestureRecognizer.numberOfTouches), OBJC_ASSOCIATION_RETAIN);
+    objc_setAssociatedObject(self, scrollTouchPoint, [NSValue valueWithCGPoint:[self.panGestureRecognizer locationInView:self]], OBJC_ASSOCIATION_RETAIN);
+}
+
+- (BOOL)M5_scrollViewWillEndDraggingWithDeceleration:(BOOL)flicked {
+    objc_setAssociatedObject(self, scrollTouchVelocity, [NSValue valueWithCGPoint:CGPointMake([[self valueForKey:@"_horizontalVelocity"] doubleValue], [[self valueForKey:@"_verticalVelocity"] doubleValue])], OBJC_ASSOCIATION_RETAIN);
+    
+    return [self M5_scrollViewWillEndDraggingWithDeceleration:flicked];
 }
 
 - (void)M5_scrollViewDidEndDraggingWithDeceleration:(BOOL)flicked {
-    if (flicked && [objc_getAssociatedObject(self, M5_scrollTouchCount) unsignedIntegerValue] == 2 && self.isScrollEnabled && !self.isPagingEnabled && ![[self valueForKey:@"_horizontalVelocity"] doubleValue]) {
-        int scrollDirection = [[self valueForKey:@"_verticalVelocity"] doubleValue] > 0 ? 1 : -1;
+    if (active && self.scrollEnabled && !self.isPagingEnabled) {
+        CGPoint velocity = [objc_getAssociatedObject(self, scrollTouchVelocity) CGPointValue];
+        NSUInteger touchCount = [objc_getAssociatedObject(self, scrollTouchCount) unsignedIntegerValue];
         
-        CGPoint scrollLocation = [objc_getAssociatedObject(self, M5_scrollTouchPoint) CGPointValue];
-        
-        CGPoint contentOffset = self.contentOffset;
-        
-        if (scrollDirection > 0) {
-            contentOffset.y = scrollLocation.y;
+        if (((twoFingers && touchCount == 2) || (!twoFingers && touchCount == 1)) && fabs(velocity.y / (velocity.x + 0.0001)) > 10) {
+            CGPoint scrollLocation = [objc_getAssociatedObject(self, scrollTouchPoint) CGPointValue];
             
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [self setContentOffset:contentOffset animated:YES];
-            });
+            CGPoint contentOffset = self.contentOffset;
+            
+            if (velocity.y > 0) {
+                contentOffset.y += (scrollLocation.y - contentOffset.y - self.frame.size.height / 7);
+                
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0), dispatch_get_main_queue(), ^{
+                    [self setContentOffset:contentOffset animated:YES];
+                });
+            }
         }
     }
     
     [self M5_scrollViewDidEndDraggingWithDeceleration:flicked];
 }
 
-+ (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        M5SwizzleMethod(self, @selector(_scrollViewDidEndDraggingWithDeceleration:), @selector(M5_scrollViewDidEndDraggingWithDeceleration:));
-        
-        M5SwizzleMethod(self, @selector(_scrollViewWillBeginDragging), @selector(M5_scrollViewWillBeginDragging));
-    });
-}
+static BOOL active;
+static BOOL twoFingers;
 
-@end
-
-@interface M5FlickScroll : NSObject
-    
-@end
-
-@implementation M5FlickScroll
-
-- (instancetype)init {
-    if (self = [super init]) {
-        NSLog(@"FlickScroll init-ed.");
+static void preferencesChanged(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
+    NSDictionary *prefs;
+    if (!(prefs = [[NSDictionary alloc] initWithContentsOfFile:@"/var/mobile/Library/Preferences/com.mhuusko5.FlickScroll.plist"])) {
+        [prefs writeToFile:@"/var/mobile/Library/Preferences/com.mhuusko5.FlickScroll.plist" atomically:YES];
+        prefs = [[NSDictionary alloc] initWithContentsOfFile:@"/var/mobile/Library/Preferences/com.mhuusko5.FlickScroll.plist"];
     }
     
-    return self;
-}
-
-+ (instancetype)sharedInstance {
-    static id sharedInstance;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedInstance = self.new;
-    });
-    return sharedInstance;
+    if (prefs[@"Active"]) {
+        active = [prefs[@"Active"] boolValue];
+    } else {
+        active = YES;
+    }
+    
+    if (prefs[@"TwoFinger"]) {
+        twoFingers = [prefs[@"TwoFinger"] boolValue];
+    } else {
+        twoFingers = NO;
+    }
+    
+    if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_7_1 && name) {
+        CFPreferencesAppSynchronize(CFSTR("com.mhuusko5.FlickScroll"));
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            preferencesChanged(NULL, NULL, NULL, NULL, NULL);
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                preferencesChanged(NULL, NULL, NULL, NULL, NULL);
+            });
+        });
+    }
 }
 
 + (void)load {
-    [self sharedInstance];
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        M5SwizzleMethod(self, @selector(_scrollViewWillBeginDragging), @selector(M5_scrollViewWillBeginDragging));
+        
+        M5SwizzleMethod(self, @selector(_scrollViewWillEndDraggingWithDeceleration:), @selector(M5_scrollViewWillEndDraggingWithDeceleration:));
+        
+        M5SwizzleMethod(self, @selector(_scrollViewDidEndDraggingWithDeceleration:), @selector(M5_scrollViewDidEndDraggingWithDeceleration:));
+        
+        preferencesChanged(NULL, NULL, CFSTR(""), NULL, NULL);
+        CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (CFNotificationCallback)preferencesChanged, CFSTR("com.mhuusko5.FlickScroll-preferencesChanged"), NULL, CFNotificationSuspensionBehaviorCoalesce);
+    });
 }
 
 @end
